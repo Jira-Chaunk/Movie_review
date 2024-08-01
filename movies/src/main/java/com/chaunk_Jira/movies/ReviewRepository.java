706b4912855cc6013b53b8898fe3e76152ab683f
Jira-Chaunk <141048007+Jira-Chaunk@users.noreply.github.com>
2024-08-01 22:52:45 +0530

    Add files via upload

---
+++ movies/src/main/java/com/chaunk_Jira/movies/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.chaunk_Jira.movies;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.bson.types.ObjectId;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId>{
+    
+}
